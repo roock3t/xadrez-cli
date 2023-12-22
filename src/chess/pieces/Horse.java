a3933f0b8dd4e635569db9da66e78d9b5ca91ba1
roock3t <igor.carvalho.501@gmail.com>
2023-12-21 22:05:36 -0300

    Classe BoardException e programação defensiva

---
+++ src/chess/pieces/Horse.java
@@ -0,0 +1,15 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Horse extends ChessPiece {
+    public Horse(Board board, Color color) {
+        super(board, color);
+    }
+    @Override
+    public String toString(){
+        return "H";
+    }
+}
